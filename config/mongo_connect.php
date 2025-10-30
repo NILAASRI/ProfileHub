cd4d26ea80ed2299f4884c825416c83ecb3dfe2c
nilaasri <146331079+NILAASRI@users.noreply.github.com>
2025-10-30 23:28:18 +0530

    Update mongo_connect.php

--- config/mongo_connect.php
+++ config/mongo_connect.php
@@ -1,7 +1,7 @@
 <?php
 //require 'vendor/autoload.php';
 try {
-    $mongoClient = new MongoDB\Client("mongodb+srv://<username>:<password>@<cluster-url>/");
+    $mongoClient = new MongoDB\Client("mongodb+srv://sriselliprt_db_user:Nilaa%402004@profilehub.yvxrns6.mongodb.net/?appName=ProfileHub");
     $mongoDB = $mongoClient->selectDatabase("profilehub");
 } catch (Exception $e) {
     echo json_encode(["status" => "error", "message" => "MongoDB connection failed: " . $e->getMessage()]);
